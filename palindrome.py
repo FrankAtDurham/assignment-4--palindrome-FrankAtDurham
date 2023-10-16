6f6b7476fd3afd4d4245549980434fb5e4d2eaf0
FrankAtDurham <francesco.aloe@dcmail.ca>
2023-10-16 14:21:33 -0400

    Step 1 Complete

---
+++ palindrome.py
@@ -0,0 +1,7 @@
+"""
+assignment 4 palindrome
+"""
+
+def is_palindrome(value):
+    if len(value) == 0:
+        assert ValueError
---
+++ tests/test_palidrome.py
@@ -0,0 +1,10 @@
+"""
+test module for palindrome
+"""
+import sys
+sys.path.append("assign4PalindromeFrankAtDurham")
+import palindrome
+
+def test_step_1():
+    """Testing Step 1  """
+    palindrome.is_palindrome(None)
